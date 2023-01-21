a0b5d9cefcf8e63f713af041208ae4eacb449a3e
Felix Ng <felixng1022@gmail.com>
2023-01-21 15:45:26 -0800

    Google login

--- client/nwhacks2023/src/App.js
+++ client/nwhacks2023/src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
-import Dashboard from "./components/dashboard";
+import { signInWithGoogle } from "./firebase";
 
 function App() {
   return (
     <div className="App">
       <Dashboard />
+      <button onClick={signInWithGoogle}> Sign In With Google</button>
+      <h1>{localStorage.getItem("name")}</h1>
+      <h1>{localStorage.getItem("email")}</h1>
+      <h1>{localStorage.getItem("profilePic")}</h1>
     </div>
   );
 }
