7faddc47a1e310bebc7f4df79517e9d6e91ed030
Akira <akyukii@student.ubc.ca>
2023-01-22 11:19:59 -0800

    stuff

--- server/poseBackend/PoseEstimation.py
+++ server/poseBackend/PoseEstimation.py
@@ -1,188 +1,149 @@
-from os import sendfile
 import cv2
 import mediapipe as mp
 import numpy as np
 import time
-from flask import Flask, request
-from flask_cors import CORS
-import flask
-import json
-import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import storage
 
-app = Flask(__name__)
-CORS(app)
+mp_face_mesh = mp.solutions.face_mesh
+face_mesh = mp_face_mesh.FaceMesh(min_detection_confidence=0.5, min_tracking_confidence=0.5)
 
-@app.route('/urls', methods=["GET"])
-def users():
-    # request_body = request.get_json()
-    # print(request_body)
-    print("url endpoint reached...")
-    with open("urls.json", "r") as f:
-        data = json.load(f)
-        return flask.jsonify(data)
+mp_drawing = mp.solutions.drawing_utils
 
-#main function
-@app.route('/', methods=["GET", "POST"])
-def analyse():
-    url = request.get_json()["urlLink"]
-    mp_face_mesh = mp.solutions.face_mesh
-    face_mesh = mp_face_mesh.FaceMesh(min_detection_confidence=0.5, min_tracking_confidence=0.5)
+drawing_spec = mp_drawing.DrawingSpec(thickness=1, circle_radius=1)
 
-    mp_drawing = mp.solutions.drawing_utils
+cap = cv2.VideoCapture("server/poseBackend/poseVideos/Allan2.mp4")
+prevText = ""
+count = 0
 
-    drawing_spec = mp_drawing.DrawingSpec(thickness=1, circle_radius=1)
+frame_width = int(cap.get(3))
+frame_height = int(cap.get(4))
 
-    cap = cv2.VideoCapture(url)
-    prevText = ""
-    count = 0
+size = (frame_width, frame_height)
 
-    frame_width = int(cap.get(3))
-    frame_height = int(cap.get(4))
-   
-    # Init firebase with your credentials
-    cred = credentials.Certificate("server/firebase.json")
-    firebase_admin.initialize_app(cred)
+result = cv2.VideoWriter('server/poseBackend/outputVideos/out.avi', cv2.VideoWriter_fourcc(*'MJPG'), 30, size)
 
-    # Get a reference to the storage bucket
-    bucket = storage.bucket()
+while cap.isOpened():
+    success, image = cap.read()
 
-    size = (frame_width, frame_height)
+    start = time.time()
 
-    result = cv2.VideoWriter('server/poseBackend/outputVideos/out.avi', cv2.cv.CV_FOURCC(*'MJPG'), 30, size)
+    # Also convert the color space from BGR to RGB
+    if not success:
+        break
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-    # Create a new blob and upload the file
-    blob = bucket.blob("outputVideos/out.avi")
-    blob.upload_from_filename('server/poseBackend/outputVideos/out.avi')
+    # To improve performance
+    image.flags.writeable = False
+    
+    # Get the result
+    results = face_mesh.process(image)
+    
+    # To improve performance
+    image.flags.writeable = True
+    
+    # Convert the color space from RGB to BGR
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
-    print(blob.public_url)
+    img_h, img_w, img_c = image.shape
+    face_3d = []
+    face_2d = []
 
-    while cap.isOpened():
-        success, image = cap.read()
+    if results.multi_face_landmarks:
+        for face_landmarks in results.multi_face_landmarks:
+            for idx, lm in enumerate(face_landmarks.landmark):
+                if idx == 33 or idx == 263 or idx == 1 or idx == 61 or idx == 291 or idx == 199:
+                    if idx == 1:
+                        nose_2d = (lm.x * img_w, lm.y * img_h)
+                        nose_3d = (lm.x * img_w, lm.y * img_h, lm.z * 3000)
 
-        start = time.time()
+                    x, y = int(lm.x * img_w), int(lm.y * img_h)
 
-        # Also convert the color space from BGR to RGB
-        if not success:
-            break
-        
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        print("1")
+                    # Get the 2D Coordinates
+                    face_2d.append([x, y])
 
-        # To improve performance
-        image.flags.writeable = False
-        
-        # Get the result
-        results = face_mesh.process(image)
-        
-        # To improve performance
-        image.flags.writeable = True
-        
-        # Convert the color space from RGB to BGR
-        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-
-        img_h, img_w, img_c = image.shape
-        face_3d = []
-        face_2d = []
-
-        if results.multi_face_landmarks:
-            for face_landmarks in results.multi_face_landmarks:
-                for idx, lm in enumerate(face_landmarks.landmark):
-                    if idx == 33 or idx == 263 or idx == 1 or idx == 61 or idx == 291 or idx == 199:
-                        if idx == 1:
-                            nose_2d = (lm.x * img_w, lm.y * img_h)
-                            nose_3d = (lm.x * img_w, lm.y * img_h, lm.z * 3000)
-
-                        x, y = int(lm.x * img_w), int(lm.y * img_h)
-
-                        # Get the 2D Coordinates
-                        face_2d.append([x, y])
-
-                        # Get the 3D Coordinates
-                        face_3d.append([x, y, lm.z])       
-                
-                # Convert it to the NumPy array
-                face_2d = np.array(face_2d, dtype=np.float64)
-
-                # Convert it to the NumPy array
-                face_3d = np.array(face_3d, dtype=np.float64)
-
-                # The camera matrix
-                focal_length = 1 * img_w
-
-                cam_matrix = np.array([ [focal_length, 0, img_h / 2],
-                                        [0, focal_length, img_w / 2],
-                                        [0, 0, 1]])
-
-                # The distortion parameters
-                dist_matrix = np.zeros((4, 1), dtype=np.float64)
-
-                # Solve PnP
-                success, rot_vec, trans_vec = cv2.solvePnP(face_3d, face_2d, cam_matrix, dist_matrix)
-
-                # Get rotational matrix
-                rmat, jac = cv2.Rodrigues(rot_vec)
-
-                # Get angles
-                angles, mtxR, mtxQ, Qx, Qy, Qz = cv2.RQDecomp3x3(rmat)
-
-                # Get the y rotation degree
-                x = angles[0] * 360
-                y = angles[1] * 360
-                z = angles[2] * 360
+                    # Get the 3D Coordinates
+                    face_3d.append([x, y, lm.z])       
             
-                # See where the user's head tilting
-                if y < -7:
-                    text = "Looking Left"
-                elif y > 7:
-                    text = "Looking Right"
-                elif x < -7:
-                    text = "Looking Down"
-                elif x > 7:
-                    text = "Looking Up"
-                else:
-                    text = "Forward"
+            # Convert it to the NumPy array
+            face_2d = np.array(face_2d, dtype=np.float64)
 
-                if text != prevText:
-                    count += 1
-                prevText = text
+            # Convert it to the NumPy array
+            face_3d = np.array(face_3d, dtype=np.float64)
 
-                # Display the nose direction
-                nose_3d_projection, jacobian = cv2.projectPoints(nose_3d, rot_vec, trans_vec, cam_matrix, dist_matrix)
+            # The camera matrix
+            focal_length = 1 * img_w
 
-                p1 = (int(nose_2d[0]), int(nose_2d[1]))
-                p2 = (int(nose_2d[0] + y * 10) , int(nose_2d[1] - x * 10))
-                
-                cv2.line(image, p1, p2, (255, 0, 0), 3)
+            cam_matrix = np.array([ [focal_length, 0, img_h / 2],
+                                    [0, focal_length, img_w / 2],
+                                    [0, 0, 1]])
 
-                # Add the text on the image
-                cv2.putText(image, text, (20, 150), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 0), 2)
-                cv2.putText(image, "x: " + str(np.round(x,2)), (500, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
-                cv2.putText(image, "y: " + str(np.round(y,2)), (500, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
-                cv2.putText(image, "z: " + str(np.round(z,2)), (500, 150), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+            # The distortion parameters
+            dist_matrix = np.zeros((4, 1), dtype=np.float64)
 
-            end = time.time()
-            totalTime = end - start
+            # Solve PnP
+            success, rot_vec, trans_vec = cv2.solvePnP(face_3d, face_2d, cam_matrix, dist_matrix)
 
-            fps = 1 / totalTime
+            # Get rotational matrix
+            rmat, jac = cv2.Rodrigues(rot_vec)
 
-            cv2.putText(image, f'FPS: {int(fps)}', (20,450), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0,255,0), 2)
+            # Get angles
+            angles, mtxR, mtxQ, Qx, Qy, Qz = cv2.RQDecomp3x3(rmat)
 
-            mp_drawing.draw_landmarks(
-                        image=image,
-                        landmark_list=face_landmarks,
-                        connections=mp_face_mesh.FACEMESH_CONTOURS,
-                        landmark_drawing_spec=drawing_spec,
-                        connection_drawing_spec=drawing_spec)
+            # Get the y rotation degree
+            x = angles[0] * 360
+            y = angles[1] * 360
+            z = angles[2] * 360
+        
+            # See where the user's head tilting
+            if y < -8:
+                text = "Looking Left"
+            elif y > 8:
+                text = "Looking Right"
+            elif x < -8:
+                text = "Looking Down"
+            elif x > 8:
+                text = "Looking Up"
+            else:
+                text = "Forward"
 
-        result.write(image)
+            if text != prevText:
+                count += 1
+            prevText = text
 
-        if cv2.waitKey(5) == 32:
-            break
-    cap.release()
-    result.release()
-    return str(int(count/2))            
+            # Display the nose direction
+            nose_3d_projection, jacobian = cv2.projectPoints(nose_3d, rot_vec, trans_vec, cam_matrix, dist_matrix)
 
-if __name__ == "__main__":
-    app.run("localhost", 8000)
+            p1 = (int(nose_2d[0]), int(nose_2d[1]))
+            p2 = (int(nose_2d[0] + y * 10) , int(nose_2d[1] - x * 10))
+            
+            cv2.line(image, p1, p2, (255, 0, 0), 3)
+
+            # Add the text on the image
+            cv2.putText(image, text, (20, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2)
+            cv2.putText(image, "x: " + str(np.round(x,2)), (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2)
+            cv2.putText(image, "y: " + str(np.round(y,2)), (150, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2)
+            cv2.putText(image, "z: " + str(np.round(z,2)), (250, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2)
+
+        end = time.time()
+        totalTime = end - start
+
+        fps = 1 / totalTime
+
+        cv2.putText(image, f'FPS: {int(fps)}', (20,500), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+        cv2.putText(image, f'Count: {int(count/2)}', (20,550), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+
+        mp_drawing.draw_landmarks(
+                    image=image,
+                    landmark_list=face_landmarks,
+                    connections=mp_face_mesh.FACEMESH_CONTOURS,
+                    landmark_drawing_spec=drawing_spec,
+                    connection_drawing_spec=drawing_spec)
+
+    result.write(image)
+    cv2.imshow("Processed Video", image)
+
+    if cv2.waitKey(5) == 32:
+        break
+
+cap.release()
+result.release()
+print(count/2)
