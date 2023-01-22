5b26dd564399f022f6d790d85fdfcf4021a81bd2
Akira <akyukii@student.ubc.ca>
2023-01-21 23:05:27 -0800

    update 2

--- server/PoseEstimation.py
+++ server/PoseEstimation.py
@@ -1,20 +1,31 @@
 import cv2 as cv
 from cvzone.PoseModule import PoseDetector
 
-video = cv.VideoCapture('poseVideos/1.mp4')
+video = cv.VideoCapture('poseVideos/3.mp4')
 pd = PoseDetector()
-fps = video.get(cv.CAP_PROP_FPS)
-totalNoFrames = video.get(cv.CAP_PROP_FRAME_COUNT)
-durationInSeconds = totalNoFrames / fps
+count = 0
+frames = 0
 while True:
     ret, img = video.read()
-    img = pd.findPose(img)
-    lmlist, bbox = pd.findPosition(img)
+    while(ret): 
+        img = pd.findPose(img)
+        lmlist, bbox = pd.findPosition(img)
 
-    cv.imshow('frame',img)
-    key = cv.waitKey(100)
-    if key == 32:
-        cv.waitKey()
-    elif key == ord('q'):
-        break
+        print(lmlist)
+        for i in range(10):
+            if lmlist[i][3] <= 0.1:
+                count += 1
+
+        cv.imshow('frame',img)
+        key = cv.waitKey(100)
+        if key == 32:
+            cv.waitKey()
+        elif key == ord('q'):
+            break
+        frames += 1
+        ret, img = video.read()
+    print((count/frames)/10)
+    break
+video.release()
+cv.destroyAllWindows()
     
---
+++ server/PoseEstimation2.py
@@ -0,0 +1,45 @@
+import cv2
+import mediapipe as mp
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
+mp_holistic = mp.solutions.holistic
+
+# For webcam input:
+cap = cv2.VideoCapture('poseVideos/3.mp4')
+with mp_holistic.Holistic(
+    min_detection_confidence=0.5,
+    min_tracking_confidence=0.5) as holistic:
+  while cap.isOpened():
+    success, image = cap.read()
+    if not success:
+      break
+
+    # To improve performance, optionally mark the image as not writeable to
+    # pass by reference.
+    image.flags.writeable = False
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    results = holistic.process(image)
+
+    # Draw landmark annotation on the image.
+    image.flags.writeable = True
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    mp_drawing.draw_landmarks(
+        image,
+        results.face_landmarks,
+        mp_holistic.FACEMESH_CONTOURS,
+        landmark_drawing_spec=None,
+        connection_drawing_spec=mp_drawing_styles
+        .get_default_face_mesh_contours_style())
+    mp_drawing.draw_landmarks(
+        image,
+        results.pose_landmarks,
+        mp_holistic.POSE_CONNECTIONS,
+        landmark_drawing_spec=mp_drawing_styles
+        .get_default_pose_landmarks_style())
+
+    print(results.pose_landmarks.landmark)
+    
+    cv2.imshow('MediaPipe Holistic', image)
+    if cv2.waitKey(5) & 0xFF == 27:
+      break
+cap.release()
